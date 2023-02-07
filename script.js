123a65c15981b3e19b22ad783adcc4be3efd49c0
EverLookNeverSee <EverLookNeverSee@Proton.Me>
2023-02-07 22:03:22 +0330

    Make dynamic current year instead of hard coded one

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 "use strict";
+
+// Make dynamic current year instead of hard coded one:
+const yearElement = document.querySelector(".year");
+yearElement.textContent = String(new Date().getFullYear());
