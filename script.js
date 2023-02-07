b667e56fd97c46845b0307f01110f3f83333aab5
EverLookNeverSee <EverLookNeverSee@Proton.Me>
2023-02-07 22:15:23 +0330

    Make mobile navigation functional

--- script.js
+++ script.js
@@ -3,3 +3,10 @@
 // Make dynamic current year instead of hard coded one:
 const yearElement = document.querySelector(".year");
 yearElement.textContent = String(new Date().getFullYear());
+
+// Make mobile navigation functional:
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+})
