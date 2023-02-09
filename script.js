cce61068122fa755c3980b98275926cb5ab90a4d
EverLookNeverSee <EverLookNeverSee@Proton.Me>
2023-02-09 13:41:24 +0330

    Implementing smooth scrolling

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 })
+
+// Smooth scroll animation:
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    // Scroll back to top:
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      })
+      // Scroll to other links
+    } else if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({behavior:"smooth"});
+    }
+    // Close mobile navigation:
+    else if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  })
+})
